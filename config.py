b4fca9bbadd7fb32908fb9b962b150f279fafc7c
Gabriel Valencia <valenciagabriel054@gmail.com>
2026-03-21 12:13:41 -0700

    Add base Config class for CI

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 import os
 
-class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///mechanic_shop.db"
+class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
     TESTING = False
+    DEBUG = False
 
-class ProductionConfig:
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///mechanic_shop.db"
+    DEBUG = True
+
+class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
     TESTING = False
