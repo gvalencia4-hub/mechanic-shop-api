31ba5ec73d522a18f0f00268c635ece9cf55dcb8
Gabriel Valencia <valenciagabriel054@gmail.com>
2026-03-23 18:53:28 -0700

    Fix production database environment variable handling

--- config.py
+++ config.py
@@ -1,17 +1,23 @@
 import os
 
+
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
     TESTING = False
     DEBUG = False
 
+
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///mechanic_shop.db"
     DEBUG = True
 
+
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get("DATABASE_URI")
+        or os.environ.get("SQLALCHEMY_DATABASE_URI")
+    )
+    SECRET_KEY = os.environ.get("SECRET_KEY", "prod-secret")
     DEBUG = False
     TESTING = False
